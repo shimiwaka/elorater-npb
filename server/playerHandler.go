673307667180212c4f66cda96cfcc8ddbdf5890e
shimiwaka <kei_ikeda@r.recruit.co.jp>
2022-11-04 15:59:37 +0900

    show players career stats

--- server/playerHandler.go
+++ server/playerHandler.go
@@ -6,10 +6,45 @@ import (
     "strconv"
 
 	"github.com/gorilla/mux"
-	// "github.com/jinzhu/gorm"
+	"github.com/jinzhu/gorm"
 )
 
 type playerHandler struct{}
+
+func getCareerPitchingStat(p Player) PitchingStat {
+	for _, stat := range p.Pitching {
+		if stat.Year == "国内通算" {
+			return stat
+		}
+	}
+	return PitchingStat{}
+}
+
+func getCareerBattingStat(p Player) BattingStat {
+	for _, stat := range p.Batting {
+		if stat.Year == "国内通算" {
+			return stat
+		}
+	}
+	return BattingStat{}
+}
+
+func getPlayerAllStats(db *gorm.DB, id int) (Player, error) {
+	var player Player
+	err := db.Model(&Player{}).Preload("Pitching").Preload("Batting").First(&player, id).Error
+	return player, err
+}
+
+func showCareer(p Player) string {
+	pStat := getCareerPitchingStat(p)
+	bStat := getCareerBattingStat(p)
+	ret := fmt.Sprintf("%s %s\n%d勝%d敗 %.2f\n%.3f %d本 %d打点",
+						p.Name, p.Birth,
+						pStat.Win, pStat.Lose, pStat.Era,
+						bStat.Avg, bStat.HR, bStat.RBI)
+
+	return ret
+}
 
 func (p *playerHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
@@ -20,8 +55,12 @@ func (p *playerHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	var player Player
 
-	db.First(&player, id)
+	player, err := getPlayerAllStats(db, id)
 
-	fmt.Fprintf(w, "%s", player)
+	if err != nil {
+		panic("failed to get player")
+	}
+
+	fmt.Fprintf(w, "%s", showCareer(player))
 
 }
